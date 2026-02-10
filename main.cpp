e3ae74282cad0f72ebebdd14fe1566b8e76aeeb8
Tadousas22 <syvokastadas21@gmail.com>
2026-02-10 13:27:53 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,24 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string v = "Tadas";
+    string p = "Šyvokas";
+    string a = "19";
+    string g = "IS25";
+    string k = "1 Kursas";
+    string st = "Informacijos sistemos ir sauga";
+    
+    cout <<" " << v << " " << p << " " << a << " " << g << " " << k << " " << st;
+    return 0;
+}
