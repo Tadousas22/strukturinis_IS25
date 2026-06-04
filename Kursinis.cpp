059c6e5bba79fe5233098251852b4d7c8b573fa5
Tadousas22 <syvokastadas21@gmail.com>
2026-06-04 23:58:56 +0300

    Update Kursinis.cpp

--- Kursinis.cpp
+++ Kursinis.cpp
@@ -7,8 +7,6 @@
 using namespace std;
 using namespace chrono;
 
-
-
 struct Statistika
 {
     long long palyginimai = 0;
@@ -17,33 +15,29 @@ struct Statistika
 };
 
 
-void iterpimoRikiavimas(vector<int>& masyvas,
-                        Statistika& statistika)
+void selectionRikiavimas(vector<int>& masyvas, Statistika& statistika)
 {
-    int dydis = masyvas.size();
+    int n = masyvas.size();
 
-    for (int i = 1; i < dydis; i++)
+    for (int i = 0; i < n - 1; i++)
     {
-        int raktas = masyvas[i];
-        int j = i - 1;
+        int minIndex = i;
 
-        while (j >= 0)
+        for (int j = i + 1; j < n; j++)
         {
             statistika.palyginimai++;
 
-            if (masyvas[j] > raktas)
+            if (masyvas[j] < masyvas[minIndex])
             {
-                masyvas[j + 1] = masyvas[j];
-                statistika.sukeitimai++;
-                j--;
-            }
-            else
-            {
-                break;
+                minIndex = j;
             }
         }
 
-        masyvas[j + 1] = raktas;
+        if (minIndex != i)
+        {
+            swap(masyvas[i], masyvas[minIndex]);
+            statistika.sukeitimai++;
+        }
     }
 }
 
@@ -62,83 +56,51 @@ void suliejimas(vector<int>& masyvas,
     vector<int> desinePuse(desiniojoDydis);
 
     for (int i = 0; i < kairiojoDydis; i++)
-    {
         kairePuse[i] = masyvas[kaire + i];
-    }
 
     for (int i = 0; i < desiniojoDydis; i++)
-    {
         desinePuse[i] = masyvas[vidurys + 1 + i];
-    }
 
-    int i = 0;
-    int j = 0;
-    int k = kaire;
+    int i = 0, j = 0, k = kaire;
 
     while (i < kairiojoDydis && j < desiniojoDydis)
     {
         statistika.palyginimai++;
 
         if (kairePuse[i] <= desinePuse[j])
-        {
-            masyvas[k] = kairePuse[i];
-            i++;
-        }
+            masyvas[k++] = kairePuse[i++];
         else
-        {
-            masyvas[k] = desinePuse[j];
-            j++;
-        }
+            masyvas[k++] = desinePuse[j++];
 
         statistika.sukeitimai++;
-        k++;
     }
 
     while (i < kairiojoDydis)
     {
-        masyvas[k] = kairePuse[i];
-        i++;
-        k++;
-
+        masyvas[k++] = kairePuse[i++];
         statistika.sukeitimai++;
     }
 
     while (j < desiniojoDydis)
     {
-        masyvas[k] = desinePuse[j];
-        j++;
-        k++;
-
+        masyvas[k++] = desinePuse[j++];
         statistika.sukeitimai++;
     }
 }
 
-
-
-void suliejimoRikiavimas(vector<int>& masyvas,
-                         int kaire,
-                         int desine,
-                         Statistika& statistika)
+void mergeRikiavimas(vector<int>& masyvas,
+                     int kaire,
+                     int desine,
+                     Statistika& statistika)
 {
     if (kaire < desine)
     {
         int vidurys = kaire + (desine - kaire) / 2;
 
-        suliejimoRikiavimas(masyvas,
-                            kaire,
-                            vidurys,
-                            statistika);
+        mergeRikiavimas(masyvas, kaire, vidurys, statistika);
+        mergeRikiavimas(masyvas, vidurys + 1, desine, statistika);
 
-        suliejimoRikiavimas(masyvas,
-                            vidurys + 1,
-                            desine,
-                            statistika);
-
-        suliejimas(masyvas,
-                   kaire,
-                   vidurys,
-                   desine,
-                   statistika);
+        suliejimas(masyvas, kaire, vidurys, desine, statistika);
     }
 }
 
@@ -148,15 +110,12 @@ vector<int> generuotiAtsitiktiniusDuomenis(int dydis)
 {
     vector<int> duomenys(dydis);
 
-    random_device atsitiktinisIrenginys;
-    mt19937 generatorius(atsitiktinisIrenginys());
-
-    uniform_int_distribution<> paskirstymas(1, 1000000);
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<> dist(1, 1000000);
 
     for (int i = 0; i < dydis; i++)
-    {
-        duomenys[i] = paskirstymas(generatorius);
-    }
+        duomenys[i] = dist(gen);
 
     return duomenys;
 }
@@ -166,9 +125,7 @@ vector<int> generuotiSurikiuotusDuomenis(int dydis)
     vector<int> duomenys(dydis);
 
     for (int i = 0; i < dydis; i++)
-    {
         duomenys[i] = i + 1;
-    }
 
     return duomenys;
 }
@@ -178,103 +135,78 @@ vector<int> generuotiAtvirkstiniusDuomenis(int dydis)
     vector<int> duomenys(dydis);
 
     for (int i = 0; i < dydis; i++)
-    {
         duomenys[i] = dydis - i;
-    }
 
     return duomenys;
 }
 
 
 
-Statistika vykdytiIterpimoRikiavima(vector<int> duomenys)
+Statistika vykdytiSelection(vector<int> duomenys)
 {
-    Statistika statistika;
+    Statistika s;
 
-    auto pradzia = high_resolution_clock::now();
+    auto start = high_resolution_clock::now();
 
-    iterpimoRikiavimas(duomenys, statistika);
+    selectionRikiavimas(duomenys, s);
 
-    auto pabaiga = high_resolution_clock::now();
+    auto end = high_resolution_clock::now();
 
-    statistika.laikasMikrosekundemis =
-        duration_cast<microseconds>
-        (pabaiga - pradzia).count();
+    s.laikasMikrosekundemis =
+        duration_cast<microseconds>(end - start).count();
 
-    return statistika;
+    return s;
 }
 
-
-
-Statistika vykdytiSuliejimoRikiavima(vector<int> duomenys)
+Statistika vykdytiMerge(vector<int> duomenys)
 {
-    Statistika statistika;
+    Statistika s;
 
-    auto pradzia = high_resolution_clock::now();
+    auto start = high_resolution_clock::now();
 
-    suliejimoRikiavimas(
-        duomenys,
-        0,
-        duomenys.size() - 1,
-        statistika);
+    mergeRikiavimas(duomenys, 0, duomenys.size() - 1, s);
 
-    auto pabaiga = high_resolution_clock::now();
+    auto end = high_resolution_clock::now();
 
-    statistika.laikasMikrosekundemis =
-        duration_cast<microseconds>
-        (pabaiga - pradzia).count();
+    s.laikasMikrosekundemis =
+        duration_cast<microseconds>(end - start).count();
 
-    return statistika;
+    return s;
 }
 
-
-
-Statistika skaiciuotiVidurki(
-    const vector<Statistika>& rezultatai)
+Statistika skaiciuotiVidurki(const vector<Statistika>& rez)
 {
-    Statistika vidurkis;
+    Statistika v;
 
-    for (const auto& rezultatas : rezultatai)
+    for (auto r : rez)
     {
-        vidurkis.palyginimai += rezultatas.palyginimai;
-        vidurkis.sukeitimai += rezultatas.sukeitimai;
-        vidurkis.laikasMikrosekundemis +=
-            rezultatas.laikasMikrosekundemis;
+        v.palyginimai += r.palyginimai;
+        v.sukeitimai += r.sukeitimai;
+        v.laikasMikrosekundemis += r.laikasMikrosekundemis;
     }
 
-    vidurkis.palyginimai /= rezultatai.size();
-    vidurkis.sukeitimai /= rezultatai.size();
-    vidurkis.laikasMikrosekundemis /=
-        rezultatai.size();
+    v.palyginimai /= rez.size();
+    v.sukeitimai /= rez.size();
+    v.laikasMikrosekundemis /= rez.size();
 
-    return vidurkis;
+    return v;
 }
 
-
-
-void vykdytiTesta(const vector<int>& pradinisMasyvas,
-                  const string& duomenuTipas)
+void vykdytiTesta(const vector<int>& duomenys, const string& tipas)
 {
-    vector<Statistika> iterpimoRezultatai;
-    vector<Statistika> suliejimoRezultatai;
+    vector<Statistika> selRez;
+    vector<Statistika> mergeRez;
 
     for (int i = 0; i < 5; i++)
     {
-        iterpimoRezultatai.push_back(
-            vykdytiIterpimoRikiavima(pradinisMasyvas));
-
-        suliejimoRezultatai.push_back(
-            vykdytiSuliejimoRikiavima(pradinisMasyvas));
+        selRez.push_back(vykdytiSelection(duomenys));
+        mergeRez.push_back(vykdytiMerge(duomenys));
     }
 
-    Statistika iterpimoVidurkis =
-        skaiciuotiVidurki(iterpimoRezultatai);
+    Statistika sel = skaiciuotiVidurki(selRez);
+    Statistika mer = skaiciuotiVidurki(mergeRez);
 
-    Statistika suliejimoVidurkis =
-        skaiciuotiVidurki(suliejimoRezultatai);
-
-    cout << duomenuTipas << endl;
- 
+    cout << "\n" << tipas << endl;
 
     cout << left
          << setw(20) << "Algoritmas"
@@ -284,17 +216,17 @@ void vykdytiTesta(const vector<int>& pradinisMasyvas,
          << endl;
 
     cout << left
-         << setw(20) << "Iterpimo"
-         << setw(20) << iterpimoVidurkis.laikasMikrosekundemis
-         << setw(20) << iterpimoVidurkis.palyginimai
-         << setw(20) << iterpimoVidurkis.sukeitimai
+         << setw(20) << "Selection"
+         << setw(20) << sel.laikasMikrosekundemis
+         << setw(20) << sel.palyginimai
+         << setw(20) << sel.sukeitimai
          << endl;
 
     cout << left
-         << setw(20) << "Suliejimo"
-         << setw(20) << suliejimoVidurkis.laikasMikrosekundemis
-         << setw(20) << suliejimoVidurkis.palyginimai
-         << setw(20) << suliejimoVidurkis.sukeitimai
+         << setw(20) << "Merge"
+         << setw(20) << mer.laikasMikrosekundemis
+         << setw(20) << mer.palyginimai
+         << setw(20) << mer.sukeitimai
          << endl;
 }
 
@@ -302,41 +234,22 @@ void vykdytiTesta(const vector<int>& pradinisMasyvas,
 
 int main()
 {
-    vector<int> dydziai =
-    {
-        5000,
-        10000,
-        50000
-    };
+    vector<int> dydziai = {5000, 10000, 50000};
 
     cout << fixed << setprecision(2);
 
     for (int dydis : dydziai)
     {
-        cout << "\n\n";
-        cout << "Duomenu kiekis: " << dydis << endl;
-  
+        cout << "\n============================\n";
+        cout << "Dydis: " << dydis << endl;
 
-        vector<int> atsitiktiniaiDuomenys =
-            generuotiAtsitiktiniusDuomenis(dydis);
+        auto ats = generuotiAtsitiktiniusDuomenis(dydis);
+        auto sur = generuotiSurikiuotusDuomenis(dydis);
+        auto atm = generuotiAtvirkstiniusDuomenis(dydis);
 
-        vector<int> surikiuotiDuomenys =
-            generuotiSurikiuotusDuomenis(dydis);
-
-        vector<int> atvirkstiniaiDuomenys =
-            generuotiAtvirkstiniusDuomenis(dydis);
-
-        vykdytiTesta(
-            atsitiktiniaiDuomenys,
-            "Atsitiktiniai duomenys");
-
-        vykdytiTesta(
-            atvirkstiniaiDuomenys,
-            "Atvirksciai surikiuoti duomenys");
-
-        vykdytiTesta(
-            surikiuotiDuomenys,
-            "Jau surikiuoti duomenys");
+        vykdytiTesta(ats, "Atsitiktiniai duomenys");
+        vykdytiTesta(atm, "Atvirksciai surikiuoti");
+        vykdytiTesta(sur, "Jau surikiuoti");
     }
 
     return 0;
